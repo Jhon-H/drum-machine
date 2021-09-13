f4c3ec7c1f641ee212a712f420ff02f7b8c47209
Jhon Jairo Hernandez <amonzos@protonmail.com>
2021-09-13 01:05:12 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function sound (props) {
+  <audio src={props.sound}></audio>
+}
+
+class Keys extends React.Component {
+
+  handleKeyUp = event => {
+    console.log(event.target);
+  }
+
+  changeName = () => {
+    sound(this.props.sound);
+    this.props.changeSoundName(this.props.let);
+  }
+
+  render () {
+    return (
+      <button onClick={this.changeName} className="key">
+        {this.props.let}
+      </button>
+    )
+  }
+}
+
+class Board extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      currSoundName: 'add'
+    }
+  }
+
+  changeSoundName = (soundName) => {
+    this.setState({currSoundName: soundName});
+  }
+
+  render () {
+    return (
+      <div className="app-board">
+        <div className="board">
+          <Keys let="Q" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="W" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="E" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="A" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="S" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="D" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="Z" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="X" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+          <Keys let="C" sound="MUSIC" changeSoundName={this.changeSoundName}/>
+        </div>
+        <div className="text">
+          <p className="sound-name"> {this.state.currSoundName} </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  constructor () {
+    this.state = {
+      
+    }
+  }
+
+  handleKeyUp = event => {
+    
+  }
+
+  render () {
+    return (
+      <div className="app" onKeyUp={this.handleKeyUp}>
+        <Board />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render (
+  <div className="all"> <App/> </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
